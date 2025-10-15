120a5eb4483233e6a720d6193ccf7d2bc3d48459
Nithisha <reddyknithisha@gmail.com>
2025-10-15 19:13:38 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var indexRouter = require('./routes/index');
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit:'5mb'}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'portfolio_secret_key_v4', resave: false, saveUninitialized: true }));
+app.use('/', indexRouter);
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error', { title: 'Error Page' });
+});
+module.exports = app;
